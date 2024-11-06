88c83d9d7b081b3aebb2e7dabd161d6ee384abcc
JanineAnn Buenaseda <buen0024@algonquinlive.com>
2024-11-06 18:11:25 -0500

    Add first horizontal list view with external images

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,46 @@ class ListScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home Page'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Image.network(
+                      'https://static.vecteezy.com/system/resources/previews/030/258/039/large_2x/beautiful-nature-wallpaper-hd-wallpaper-ai-generated-free-photo.jpg',
+                      width: 250,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Image.network(
+                      'https://m.media-amazon.com/images/I/81MClsmL6TL._SL500_.jpg',
+                      width: 250,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Image.network(
+                      'https://img.freepik.com/premium-photo/wide-angle-shot-single-tree-growing-clouded-sky-sunset-surrounded-by-grass_181624-22807.jpg?semt=ais_hybrid',
+                      width: 250,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
     );
   }
 }
